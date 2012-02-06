0b782b98bf81ec73b480e6ea10cab01c21f8724a
Laurent Bigonville <bigon@bigon.be>
2012-02-06 10:40:08 +0100

    Imported Upstream version 0.9.5

--- lib/vagrant/driver/virtualbox.rb
+++ lib/vagrant/driver/virtualbox.rb
@@ -94,6 +94,7 @@ module Vagrant
                                :read_used_ports,
                                :read_vms,
                                :set_mac_address,
+                               :set_name,
                                :share_folders,
                                :ssh_port,
                                :start,
@@ -117,7 +118,12 @@ module Vagrant
 
         # Note: We split this into multiple lines because apparently "".split("_")
         # is [], so we have to check for an empty array in between.
-        parts = execute("--version").split("_")
+        output = execute("--version")
+        if output =~ /vboxdrv kernel module is not loaded/
+          raise Errors::VirtualBoxKernelModuleNotLoaded
+        end
+
+        parts = output.split("_")
         return nil if parts.empty?
         parts[0].split("r")[0]
       end
